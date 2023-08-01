a1fce3774cb44347dc7dddb1cabdb09d4837d218
Caroline A Bandeira <carol_bandeira7@hotmail.com>
2023-08-01 16:44:40 -0300

    commit

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-next-button",
-    prevEl: ".swiper-prev-button" },
-
-  effect: "fade",
-  loop: "infinite",
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction" } });
-
-
-
-swiper.on('slideChange', function (sld) {
-  document.body.setAttribute('data-sld', sld.realIndex);
-});
+function toggleMenu() {
+  var menu = document.querySelector('.header-menu');
+  if (menu.style.display === "block") {
+      menu.style.display = "none";
+  } else {
+      menu.style.display = "block";
+  }
+}
