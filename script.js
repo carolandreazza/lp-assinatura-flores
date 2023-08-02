c71a6ee136bdc2a1270be74a731ed4e23b7feb34
Caroline A Bandeira <carol_bandeira7@hotmail.com>
2023-08-02 15:23:03 -0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
+// Função para alternar a visibilidade do menu hamburguer
 function toggleMenu() {
   var menu = document.querySelector('.header-menu');
-  if (menu.style.display === "block") {
-      menu.style.display = "none";
-  } else {
-      menu.style.display = "block";
-  }
+  menu.classList.toggle('show');
 }
